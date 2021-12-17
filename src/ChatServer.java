58c1a6d6612814c2b1fa9bdfbd0345a22dba08d3
Amar05 <a.walid20010@gmail.com>
2021-12-17 20:04:15 +0300

    Initial commit

---
+++ src/ChatServer.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        while (true){
+            int text = in.read();
+
+            System.out.println(text);
+            out.println(text);
+        }
+
+
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChatServer server = new ChatServer();
+        server.start(9563);
+    }
+}
